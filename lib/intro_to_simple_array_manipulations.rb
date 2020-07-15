0d98bd0b31e665870b06688cf816739612b27cd7
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-15 22:57:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(array1,array2)
   array1.concat(array2)
 end
 
-def using_inser(array1,element)
-  array1.insert(4,element)
+def using_inser(array,element)
+  array.insert(3,element)
 end
